854537e7a99fd8076318fa89f577f127764abfd9
youqiting <48447989+youqiting@users.noreply.github.com>
2019-04-15 10:27:15 +0800

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  mounted:function(){
+    const this_ = this;
+      window.onreSize =()=>{
+          return (()=>{
+              window.screenWidth = document.body.clientWidth;
+              this_.screenWidth = window.screenWidth;
+          })()
+      },
+      this.$nextTick(function(){
+        window.onresize;
+      })
+  }
+}
+</script>
+
+<style>
+
+</style>
