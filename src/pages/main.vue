c01c03a78b68c6d2d3711f5ea6857e806e4b8c1f
youqiting <48447989+youqiting@users.noreply.github.com>
2019-04-18 00:05:02 +0800

    Update main.vue

--- src/pages/main.vue
+++ src/pages/main.vue
@@ -1,50 +1,50 @@
 <template>
-  <div id="main">
+    <div id="main">
+        <el-container>
+            <el-aside width="200px">
+                <Aside/>
+            </el-aside>
 
-    <el-container>
-      <el-aside width="200px">
-        <Aside />
-      </el-aside>
+            <el-container>
+                <el-header>
+                    <Header/>
+                </el-header>
 
-      <el-container>
-        <el-header>
-          <Header />
-        </el-header>
+                <el-main>
+                    <router-view></router-view>
+                </el-main>
 
-        <el-main>
-          <router-view></router-view>
-        </el-main>
-
-        <el-footer>
-          <Footer />
-        </el-footer>
-      </el-container>
-  </el-container>
-  </div>
+                <el-footer>
+                    <Footer/>
+                </el-footer>
+            </el-container>
+        </el-container>
+    </div>
 </template>
 
 <script>
-import Aside from '../components/aside.vue'
-import Header from '../components/header.vue'
-import Footer from '../components/footer.vue'
+import Aside from "../components/aside.vue";
+import Header from "../components/header.vue";
+import Footer from "../components/footer.vue";
 
 export default {
-  name: 'app',
-  components:{
-    Aside, Header,Footer
-  }
-}
+    name: "app",
+    components: {
+        Aside,
+        Header,
+        Footer
+    },
+};
 </script>
 
 <style>
-.el-aside{
-  height: 100%;
-  text-align: left;
-  color: rgb(87, 84, 82);
+.el-aside {
+    height: 100vh;
+    text-align: left;
 }
-.el-main{
-  width: 100%;
-  height: auto;
-  margin-bottom: 50px;
+.el-main {
+    width: 100%;
+    height: auto;
+    margin-bottom: 50px;
 }
 </style>
