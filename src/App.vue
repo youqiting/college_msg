9608951d2ec56dfc9c338448d6d14dd9c1da422a
youqiting <48447989+youqiting@users.noreply.github.com>
2019-04-18 00:06:18 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,24 @@
 <template>
-  <router-view></router-view>
+    <router-view></router-view>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  mounted:function(){
-    const this_ = this;
-      window.onreSize =()=>{
-          return (()=>{
-              window.screenWidth = document.body.clientWidth;
-              this_.screenWidth = window.screenWidth;
-          })()
-      },
-      this.$nextTick(function(){
-        window.onresize;
-      })
-  }
-}
+    name: "app",
+    mounted: function() {
+        const this_ = this;
+        (window.onreSize = () => {
+            return (() => {
+                window.screenWidth = document.body.clientWidth;
+                this_.screenWidth = window.screenWidth;
+            })();
+        }),
+        this.$nextTick(function() {
+            window.onresize;
+        });
+    }
+};
 </script>
 
 <style>
-
 </style>
