09c4b118643476b986692430d2acdfaa6a60ea83
youqiting <48447989+youqiting@users.noreply.github.com>
2019-04-18 00:06:22 +0800

    Update aside.vue

--- src/components/aside.vue
+++ src/components/aside.vue
@@ -1,23 +1,24 @@
 <template>
     <div class="aside_">
-        <div class="aside_top">
-            <div><img src="../imgs/icon_main.png"/></div>
-            <div>
-                首页
-            </div>
-        </div>
+        <!-- 学生 menu -->
         <el-menu 
-            default-active="2"
-            background-color="rgba(128, 159, 247, 0.61)"
+            :unique-opened="true"
+            :default-active="this.$router.path"
+            background-color="rgba(128, 159, 247)"
             text-color="#fff"
-            >
+            v-if="this.usertype == 'student'"
+            router>
+            <el-menu-item index="/main">
+                <img src="../imgs/icon_main.png"/>
+                <span>首页</span>
+            </el-menu-item>
             <el-submenu index="1">
                 <template slot="title" class="title">
                     <img src="../imgs/icon_man.png"/>
                     <span>实习</span>
                 </template>
-                <el-menu-item index="1-1">
-                    <router-link :to="'/practiceInfo'" class="unline">实习信息</router-link>
+                <el-menu-item index="/practiceInfo">
+                    <span class="item_title">实习信息</span>
                 </el-menu-item>
             </el-submenu>
             <el-submenu index="2">
@@ -25,11 +26,11 @@
                     <img src="../imgs/icon_man.png"/>
                     <span>毕业设计</span>
                 </template>
-                <el-menu-item index="1-1">
-                    <router-link :to="'/myDesign'" class="unline">我的选题</router-link>
+                <el-menu-item index="/myDesign">
+                    <span class="item_title">我的选题</span>
                 </el-menu-item>
-                <el-menu-item index="1-2">
-                    <router-link :to="'/allDesign'" class="unline">所有题目</router-link>
+                <el-menu-item index="/allDesign">
+                    <span class="item_title">所有题目</span>
                 </el-menu-item>
             </el-submenu>
             <el-submenu index="3">
@@ -37,29 +38,21 @@
                     <img src="../imgs/icon_man.png"/>
                     <span>论坛</span>
                 </template>
-                <el-menu-item index="1-1">
-                    <router-link :to="'/'" class="unline">所有话题</router-link>
+                <el-menu-item index="/">
+                    <span class="item_title">所有话题</span>
                 </el-menu-item>
             </el-submenu>
-            <el-submenu index="4">
-                <template slot="title">
-                    <img src="../imgs/icon_man.png"/>
-                    <span>招聘</span>
-                </template>
-                <el-menu-item index="1-1">
-                    <router-link :to="'/'" class="unline">招聘列表</router-link>
-                </el-menu-item>
-                <el-menu-item index="1-2">
-                    <router-link :to="'/'" class="unline">发布招聘</router-link>
-                </el-menu-item>
-            </el-submenu>
+            <el-menu-item index="/recruit">
+                <img src="../imgs/icon_man.png"/>
+                <span slot="title">查看招聘信息</span>
+            </el-menu-item>
             <el-submenu index="5">
                 <template slot="title">
                     <img src="../imgs/icon_man.png"/>
                     <span>校园新闻</span>
                 </template>
-                <el-menu-item index="1-1">
-                    <router-link :to="'/'" class="unline" >校园风采</router-link>
+                <el-menu-item index="/news">
+                    <span class="item_title" >校园风采</span>
                 </el-menu-item>
             </el-submenu>
             <el-submenu index="6">
@@ -67,12 +60,85 @@
                     <img src="../imgs/icon_man.png"/>
                     <span>班级名单</span>
                 </template>
-                <el-menu-item index="1-1">
-                    <router-link :to="'/'" class="unline">花名册</router-link>
+                <el-menu-item index="/classForm">
+                    <span class="item_title">花名册</span>
                 </el-menu-item>
             </el-submenu>
         </el-menu>
-    
+
+        <!-- 教师 menu -->
+        <el-menu 
+            :unique-opened="true"
+            :default-active="this.$router.path"
+            background-color="rgba(128, 159, 247)"
+            text-color="#fff"
+            v-if="this.usertype == 'teacher'"
+            router>
+            <el-menu-item index="/main">
+                <img src="../imgs/icon_main.png"/>
+                <span>首页</span>
+            </el-menu-item>
+            <el-submenu index="1">
+                <template slot="title" class="title">
+                    <img src="../imgs/icon_man.png"/>
+                    <span>信息</span>
+                </template>
+                <el-menu-item index="/teacherInfo">
+                    <span class="item_title">个人信息</span>
+                </el-menu-item>
+            </el-submenu>
+            <el-submenu index="2">
+                <template slot="title">
+                    <img src="../imgs/icon_man.png"/>
+                    <span>毕业设计</span>
+                </template>
+                <el-menu-item index="/myDesign">
+                    <span class="item_title">学生选题</span>
+                </el-menu-item>
+                <el-menu-item index="/allDesign">
+                    <span class="item_title">所有题目</span>
+                </el-menu-item>
+            </el-submenu>
+            <el-submenu index="3">
+                <template slot="title">
+                    <img src="../imgs/icon_man.png"/>
+                    <span>论坛</span>
+                </template>
+                <el-menu-item index="/">
+                    <span class="item_title">所有话题</span>
+                </el-menu-item>
+            </el-submenu>
+            <el-submenu index="4">
+                <template slot="title">
+                    <img src="../imgs/icon_man.png"/>
+                    <span>招聘消息</span>
+                </template>
+                <el-menu-item index="/recruit">
+                    <span class="item_title">查看招聘信息</span>
+                </el-menu-item>
+                <el-menu-item index="/">
+                    <span class="item_title">发布招聘</span>
+                </el-menu-item>
+            </el-submenu>
+            <el-submenu index="5">
+                <template slot="title">
+                    <img src="../imgs/icon_man.png"/>
+                    <span>校园新闻</span>
+                </template>
+                <el-menu-item index="/news">
+                    <span class="item_title" >校园风采</span>
+                </el-menu-item>
+            </el-submenu>
+            <el-submenu index="6">
+                <template slot="title">
+                    <img src="../imgs/icon_man.png"/>
+                    <span>班级名单</span>
+                </template>
+                <el-menu-item index="/classForm">
+                    <span class="item_title">花名册</span>
+                </el-menu-item>
+            </el-submenu>
+        </el-menu>
     </div>
 </template>
 
@@ -80,37 +146,30 @@
 
 export default {
   name: 'aside_',
-  
+  data(){
+      return{
+          usertype: localStorage.getItem('usertype')
+      }
+  }
 }
 </script>
 
 <style>
-.aside_{
-    margin-top: 60px;
-}
-.aside_top{
-    padding: 10px 22px;
-    color: #fff;
-    background-color: rgba(128, 159, 247, 0.61);
-}
-.aside_top  div{
-    padding-right: 15px;
-}
-.aside_top > div{
-    display: inline-block;
+.aside_ , .el-menu{
+    height: 100%;
 }
 .el-submenu__title span{
     padding-left: 10px;
 }
-.el-menu-item a{
-    display: block;
+.el-menu-item span{
+    padding-left: 10px;
+}
+.el-menu-item .item_title{
     padding-left: 20px;
+    color: aqua;
 }
-.title{
-    font-weight :bold;
-}
-.unline{
-    text-decoration: none;
-    color: rgb(255, 247, 134);
+.el-menu-item:focus, .el-menu-item:hover {
+    outline: 0;
+    background-color: #f5dd029f;
 }
 </style>
