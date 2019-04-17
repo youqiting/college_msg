c0a5a23ed0977bae71677392bc7855ed2da8d175
youqiting <48447989+youqiting@users.noreply.github.com>
2019-04-18 00:06:46 +0800

    Create classForm.vue

---
+++ src/pages/classForm.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="classForm">
+        <el-select v-model="value" placeholder="班级" @change="selectStuForm" filterable clearable>
+            <el-option
+                v-for="item in classForm"
+                :key="item.label"
+                :label="item.label"
+                :value="item.value"
+                size="mini">
+                <span>{{ item.label }}</span>
+            </el-option>
+        </el-select>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "classForm",
+    data() {
+        return {
+            value: "",
+            classForm: [
+                { label: "2014级", value: "2014" },
+                { label: "2015级", value: "2015" },
+                { label: "201672班", value: "201672" }
+            ]
+        };
+    },
+    methods: {
+        selectStuForm: function(val) {
+            console.log(val);
+            var params = {
+                grade: val
+            };
+            this.$http.post(this.$api.selectAllStu, params).then(res => {
+                console.log(res.data.result);
+            });
+        }
+    }
+};
+</script>
+
+<style>
+</style>
