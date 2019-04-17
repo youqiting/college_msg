fd14b17cb9d1ddbe324e05bd2b93404d07ca63ed
youqiting <48447989+youqiting@users.noreply.github.com>
2019-04-18 00:05:30 +0800

    teacherInfo

--- src/router.js
+++ src/router.js
@@ -3,10 +3,15 @@ import Router from 'vue-router'
 
 import main from './pages/main.vue'
 import login from './pages/login.vue'
+
 import news from './pages/news.vue'
 import practiceInfo from './pages/practiceInfo.vue'
 import myDesign from './pages/myDesign.vue'
 import allDesign from './pages/allDesign.vue'
+import recruit from './pages/recruit.vue'
+import classForm from './pages/classForm.vue'
+
+import teacherInfo from './pages/teacherInfo.vue'
 
 Vue.use(Router);
 
@@ -20,9 +25,14 @@ export default new Router({
             component:main,
             children:[
                 {path:'', component: news},
+                {path:'/news', component: news},
                 {path:'/practiceInfo', component: practiceInfo},
                 {path:'/myDesign', component: myDesign},
                 {path:'/allDesign', component: allDesign},
+                {path:'/recruit', component: recruit},
+                {path:'/classForm', component: classForm},
+
+                {path:'/teacherInfo', component: teacherInfo},
             ]
         }
     ]
