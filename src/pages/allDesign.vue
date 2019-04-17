921d3d019fed3b056c89bf39d35ee6a86cd56c9c
youqiting <48447989+youqiting@users.noreply.github.com>
2019-04-18 00:06:50 +0800

    Update allDesign.vue

--- src/pages/allDesign.vue
+++ src/pages/allDesign.vue
@@ -49,22 +49,30 @@
                 <el-col :span="10">
                     <div>{{item.design_subject}}</div>
                 </el-col>
-                <el-col :span="3">
-                    <div>&nbsp;{{item.chooseMsg}}</div>
+                <el-col :span="3" class="choose_stu">
+                    <div v-html="item.chooseMsg"></div>
                 </el-col>
                 <el-col :span="3">
-                    <div style="margin-top:10px;">
-                        <el-radio v-model="chooseDesign" @change="choose_design()">选择</el-radio>
+                    <!-- <div>
+                        <el-button type="text" :id="'choose_'+item.id" @click="choose_design(item.id)">选择</el-button>
+                    </div>  v-if="item.chose == false"-->
+                    <div>
+                        <el-button type="text" :class="{'is-disabled': chooseFlag == true }" @click="choose(item.id)">选择</el-button>
                     </div>
                 </el-col>
             </el-row>
             <el-pagination
                 @current-change="handleCurrentChange"
-                :current-page.sync="page"
-                :page-size="10"
+                :current-page.sync="currentPage"
+                :page-size="7"
                 layout="prev, pager, next, jumper"
                 :total="this.designLength">
             </el-pagination>
+            <div >
+                指导老师：{{this.stu_design.teacher_name}}<br/>
+                课题名称：{{this.stu_design.design_subject}}  
+            </div>
+            <el-button type="text" @click="selectDesignByStu()">学生课题名</el-button>
         </div>
     </div>
 </template>
@@ -74,19 +82,22 @@ export default {
     name:'allDesign',
     data(){
         return{
-            grade : localStorage.getItem('username').substring(0,4),
-            tchName:[],
-            value:'',
-            design:[],
-            chooseDesign:false,
-            page: 1 ,
-            currentPage: '',
-            designLength: 0
+            grade: localStorage.getItem('username').substring(0,4),
+            value: null,
+            oldValue: '',
+            tchNameStr: '',
+            tchName: [],
+            design: [],
+            chooseFlag:'false',
+            currentIndex: '',
+            currentPage: 1,
+            designLength: 0,
+            stu_design: {}
         }
     },
     mounted:function(){
         this.$nextTick(function(){
-            this.handleCurrentChange(this.page);
+            this.select_design();
         });
     },
     methods:{
@@ -99,50 +110,100 @@ export default {
             this.$http.post(this.$api.selectDesign, params).then(res=>{
                 var data = res.data.result;
                 this.designLength = data.length;
-                var tchNameStr= '';
                 data.forEach((item, index)=>{
                     var arr ={teacher_name:'',teacher_id:''};
-                    if(tchNameStr.indexOf(item.teacher_name)< 0){
-                        tchNameStr += item.teacher_name + ",";
+                    if(this.tchNameStr.indexOf(item.teacher_name)< 0){
+                        this.tchNameStr += item.teacher_name + ",";
                         arr.teacher_name = item.teacher_name;
                         arr.teacher_id = item.teacher_id;
                         this.tchName.push(arr);
                     }
+                    if(item.chooseMsg !=null){
+                        item.chooseMsg = item.chooseMsg.replace(/,/g, "<br/>").replace(/，/g,"<br/>");
+                    }else{
+                        item.chooseMsg = "&nbsp;";
+                    } 
+                    if(item.chooseMsg.indexOf(localStorage.getItem('name'))>= 0){
+                        this.stu_design = item;
+                        console.log(item.chooseMsg.indexOf(localStorage.getItem('name'))+",item.chooseMsg:"+item.chooseMsg);
+                        console.log("this.stu_design:"+this.stu_design);
+                    }
                 });
-                var indexStart = (this.currentPage-1) *10;
-                var indexEnd = this.currentPage *10;
+                var indexStart = (this.currentPage-1) *7;
+                var indexEnd = this.currentPage *7;
                 this.design = data.slice(indexStart, indexEnd);
             })
         },
 
         //搜索教师课题
         selectTeacher:function(val){
-            var params={
-                form: "design" + this.grade,
-                name : val
+            if(val == null){
+                this.currentPage = 1;
+                this.select_design();
+            }else{
+                if(this.oldValue != val){
+                    this.currentPage = 1;
+                }
+                var params={
+                    form: "design" + this.grade,
+                    name : val
+                }
+                this.$http.post(this.$api.selectDesignByTch, params).then(res=>{
+                    var data = res.data.result;
+                    this.designLength = data.length;
+                    data.forEach((item, index)=>{
+                        if(item.chooseMsg !=null){
+                            item.chooseMsg = item.chooseMsg.replace(/,/g, "<br />").replace(/，/g,"<br />");
+                        }else if(item.chooseMsg == '' || item.chooseMsg == null){
+                            item.chooseMsg = "&nbsp;";
+                        }
+                    });
+                    var indexStart = (this.currentPage-1) *7;
+                    var indexEnd = this.currentPage *7;
+                    this.design = data.slice(indexStart, indexEnd);
+                })
             }
-            this.$http.post(this.$api.selectDesignByTch, params).then(res=>{
-                var data = res.data.result;
-                this.designLength = data.length;
-                var indexStart = (this.currentPage-1) *10;
-                var indexEnd = this.currentPage *10;
-                this.design = data.slice(indexStart, indexEnd);
-            })
+            this.oldValue = val;
         },
 
-        //选中
-        choose_design:function(){
-            console.log(this.chooseDesign);
+        choose:function(id){
+            this.chooseFlag = true;
+            this.currentIndex = id;
+            console.log("this.currentIndex:"+ id);
+        },
+
+        //数据库更新选中值
+        updateChoose:function(id, chooseMsg){
+            var params = {
+                form: "design" + this.grade,
+                id: id,
+                chooseMsg: chooseMsg
+            }
+            this.$http.post(this.$api.updateChoose, params).then(res=>{
+                var data = res.data.result;
+                console.log(data);
+            })
         },
 
         //当前页改变时--
         handleCurrentChange(val) {
             this.currentPage = val;
-            if(this.value ==''){
+            if(this.value == null){
                 this.select_design();
             }else{
-                this.selectTeacher();
+                this.selectTeacher(this.value);
             }
+        },
+
+        //课题名称
+        selectDesignByStu:function(){
+            var params = {
+                form: "design" + this.grade, 
+                name: localStorage.getItem('name')
+            }
+            this.$http.post(this.$api.selectDesignByStu, params).then(res=>{
+                this.stu_design = res.data.result;
+            })
         }
     }
 }
@@ -165,6 +226,12 @@ export default {
     line-height: 25px;
     overflow: hidden;
     border-bottom: 1px solid #ddd;
+}
+.choose_stu{
+    font-size: 14px;
+}
+.el-pagination{
+    margin-top: 10px;
 }
 </style>
 
